c06ce295475b1533de24ff2b75b64c77485eaed5
deepak-077 <itsdepak@gmail.com>
2025-10-29 16:43:13 +0530

    todo app completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,64 @@
-import Image from "next/image";
+"use client"
 
+import { useState } from "react";
 export default function Home() {
+  
+  // State variables
+  const [newTodo,setNewTodo]=useState("")
+
+  const [todos,setTodos]=useState([
+    {id:1,title:"wake up 4:30 am", done:true},
+    {id:2,title:"Go to gym", done:false},
+    {id:3,title:"Meditate", done:false},
+    {id:4,title:"Run 3kms", done:true},
+    {id:5,title:"Study 2 hours", done:true},
+
+  ])
+
+  const completed=todos.filter(a=>a.done===true);
+  
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="bg-gray-700 h-screen text-white">
+      {/* All Todos */}
+      <div className="w-[500px] text-2xl">
+        <h1 className="bg-amber-500  ">All Todos</h1>
+        {todos.map((item,index)=>(
+          <div className="flex">
+            <p key={item.id}>{item.title}</p>
+            <button className="bg-lime-500 rounded-full p-2">Mark As Done</button>
+            <button className="bg-red-400 rounded-full p-2" onClick={()=>{
+              setTodos(todos.filter(a=>a.id!==item.id))
+            }}>Delete Todo</button>
+          </div>
+          
+
+        ))}
+      </div>
+
+      {/* Completed Todos */}
+      <div className="w-[500px] text-2xl">
+        <h1 className="bg-lime-400  ">All Todos</h1>
+        {completed.map((item,index)=>(
+          <p key={item.id} className="line-through">{item.title}</p>
+        ))}
+      </div>
+
+      {/* Add Todo */}
+      <div className="flex">
+        <input type="text" placeholder="Add new todo" value={newTodo} onChange={(e)=>{
+          setNewTodo(e.target.value)
+        }} />
+
+        <button onClick={()=>{
+          const task= { id:todos.length,title:newTodo,done:false }
+          setTodos((prev)=>[...prev,task])
+          setNewTodo("")
+
+        }}>Add Todo</button>
+      </div>
+
+
+
     </div>
   );
 }
